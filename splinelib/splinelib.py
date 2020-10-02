ec8205ae2b9771e1d6fe2669eb75fb5b241b6aeb
giorgio <giotb92@gmail.com>
2020-10-02 17:12:58 +0200

    Add debugging print option in __init__ method and add __len__ method.

--- splinelib/splinelib.py
+++ splinelib/splinelib.py
@@ -5,6 +5,7 @@ Classes and functions for cubic splines determination."""
 
 # Standard library inputs
 from typing import (List, Tuple)
+from random import seed, random
 
 # Third-party imports
 import numpy as np
@@ -15,11 +16,37 @@ import numpy as np
 class CubicSpline(object):
     """Docstring"""
 
-    def __init__(self, xlist: np.array, ylist: np.array, Mlist: np.array):
+    def __init__(self, xlist: np.array, ylist: np.array, Mlist: np.array,
+                 verbose: bool = False):
         """Data for characterizing the cubic spline:
             xlist: knots
             ylist: interpolated values
             Mlist: 2nd derivatives at the knots"""
         self.xlist = xlist
-        self.ylist = xlist
-        self.Mlist = xlist
+        self.ylist = ylist
+        self.Mlist = Mlist
+
+        # Debugging print
+        if verbose:
+            print("Created CubicSpline instance. Length = %4d" % (len(self)))
+            xout = "xlist: "
+            yout = "ylist: "
+            Mout = "Mlist: "
+            for i in range(len(self)):
+                xout += "%10.3f" % (xlist[i])
+                yout += "%10.3f" % (ylist[i])
+                Mout += "%10.3f" % (Mlist[i])
+            print(xout)
+            print(yout)
+            print(Mout)
+
+    def __len__(self):
+        return len(self.xlist)
+
+
+# if __name__ == '__main__':
+#     seed(0)
+#     xvals = [random() for _ in range(5)]
+#     yvals = [random() for _ in range(5)]
+#     Mvals = [random() for _ in range(5)]
+#     myspline = CubicSpline(xvals, yvals, Mvals, verbose=True)

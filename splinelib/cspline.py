9cbdca7f5ce462e7b4fe9c0182371f2ef76f5f78
giorgio <giotb92@gmail.com>
2020-10-08 15:20:37 +0200

    Add dict_form property and setter and reset method.

--- splinelib/cspline.py
+++ splinelib/cspline.py
@@ -16,7 +16,7 @@ import numpy as np
 class CubicSpline(object):
     """Docstring"""
 
-    def __init__(self, xlist: np.array, ylist: np.array, Mlist: np.array,
+    def __init__(self, xlist: List, ylist: List, Mlist: List,
                  verbose: bool = False):
         """Characterization of the cubic spline:
         Args:
@@ -42,6 +42,9 @@ class CubicSpline(object):
 
     def __str__(self):
         """Returns a readable representation of the spline. Used for printing.
+
+        Assumptions:
+            -- xlist, ylist an Mlist all have the same length.
         """
         xout = "xlist: "
         yout = "ylist: "
@@ -55,6 +58,26 @@ class CubicSpline(object):
         {yout}
         {Mout}""" % (len(self))
 
+    @property
+    def dict_form(self):
+        """Returns a dict representation of the spline."""
+        return {'xlist': self.xlist,
+                'ylist': self.ylist,
+                'Mlist': self.Mlist}
+
+    @dict_form.setter
+    def dict_form(self, spline_dict):
+        """Sets the spline by setting its dict representation."""
+        self.xlist = spline_dict['xlist']
+        self.ylist = spline_dict['ylist']
+        self.Mlist = spline_dict['Mlist']
+
+    def reset(self):
+        """Resets all the attributes of the spline instance."""
+        self.xlist = []
+        self.ylist = []
+        self.Mlist = []
+
 
 if __name__ == '__main__':
     seed(0)
@@ -64,3 +87,16 @@ if __name__ == '__main__':
     myspline = CubicSpline(xvals, yvals, Mvals)
     print(repr(myspline))
     print(myspline)
+
+    print(myspline.dict_form)
+
+    mydict = {
+        'xlist': [1., 2., 3.],
+        'ylist': [1.1, 2.2, 3.3],
+        'Mlist': [0., 0., 0.]
+    }
+
+    myspline.dict_form = mydict
+    print(myspline)
+    myspline.reset()
+    print(myspline.dict_form)

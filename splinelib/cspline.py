c9c47159cf911e64de3c1d516d9540c877293a9c
giorgio <giotb92@gmail.com>
2020-10-10 09:39:47 +0200

    Add the eval, log_spline and read methods (with tests).

--- splinelib/cspline.py
+++ splinelib/cspline.py
@@ -4,7 +4,10 @@
 Classes and functions for cubic splines determination."""
 
 # Standard library inputs
+import os
 from random import seed, random
+import re
+import sys
 from typing import (List, Tuple)
 
 # Third-party imports
@@ -14,7 +17,7 @@ import numpy as np
 
 
 class CubicSpline(object):
-    """Docstring"""
+    """Cubic spline data structure and functions."""
 
     def __init__(self, xlist: List, ylist: List, Mlist: List,
                  verbose: bool = False):
@@ -32,16 +35,16 @@ class CubicSpline(object):
             print(self)
 
     def __len__(self):
-        """Returns the length of the cubic spline."""
+        """Return the length of the cubic spline."""
         return len(self.xlist)
 
     def __repr__(self):
-        """Returns a representation of the spline object. Used for re-creating
+        """Return a representation of the spline object. Used for re-creating
         the object."""
         return f"CubicSpline('{self.xlist}, {self.ylist}, {self.Mlist}')"
 
     def __str__(self):
-        """Returns a readable representation of the spline. Used for printing.
+        """Return a readable representation of the spline. Used for printing.
 
         Assumptions:
             -- xlist, ylist an Mlist all have the same length.
@@ -60,43 +63,153 @@ class CubicSpline(object):
 
     @property
     def dict_form(self):
-        """Returns a dict representation of the spline."""
+        """Return a dict representation of the spline."""
         return {'xlist': self.xlist,
                 'ylist': self.ylist,
                 'Mlist': self.Mlist}
 
     @dict_form.setter
-    def dict_form(self, spline_dict):
-        """Sets the spline by setting its dict representation."""
+    def dict_form(self, spline_dict: dict):
+        """Set the spline by setting its dict representation."""
         self.xlist = spline_dict['xlist']
         self.ylist = spline_dict['ylist']
         self.Mlist = spline_dict['Mlist']
 
+    @staticmethod
+    def _log_spline(iflag, msg, log_file=None):
+        fh = open(log_file, 'a') if log_file else sys.stdout
+        if iflag == -1:
+            print("CubicSpline Error: " + msg, file=fh)
+        if iflag == 1:
+            print("CubicSpline Warning: " + msg, file=fh)
+        if fh != sys.stdout:
+            fh.close()
+
+    def eval(self, t: float, log_file=None):
+        """Evaluates the cubic spline in t.
+
+        Arguments:
+            -- t: evaluation point.
+            -- log_file (optional): file where possible errors/warnings are
+                logged."""
+        # Check on spline's data
+        if len(self) == 0:
+            iflag = -1
+            msg = "No data defined for this spline."
+            CubicSpline._log_spline(iflag, msg, log_file=log_file)
+            return None
+        elif len(self) == 1:
+            iflag = 1
+            msg = "Only one data point defined. Return te value at that point."
+            CubicSpline._log_spline(iflag, msg, log_file=log_file)
+            return self.ylist[0]
+
+        # Define initial interval
+        lo = 0
+        hi = len(self) - 1
+
+        # Check if there will be extrapolation
+        if t < self.xlist[0]:
+            i = 0
+            iflag = 1
+            msg = f"Evaluation point t = {t} is lower than x0 = {self.xlist[0]}. Extrapolating."
+            CubicSpline._log_spline(iflag, msg, log_file=log_file)
+        if t > self.xlist[-1]:
+            i = len(self) - 2
+            iflag = 1
+            msg = f"Evaluation point t = {t} is greater than xn = {self.xlist[-1]}. Extrapolating."
+            CubicSpline._log_spline(iflag, msg, log_file=log_file)
+        else:
+            iflag = 0
+            # Binary search of the index
+            while hi > lo + 1:
+                mid = (lo + hi) // 2  # integer division
+                if t < self.xlist[mid]:
+                    hi = mid
+                else:
+                    lo = mid
+            i = lo
+
+        # Definition of the interval
+        xi = self.xlist[i]
+        xii = self.xlist[i + 1]
+        h = xii - xi
+        if h < 2 * np.finfo(float).eps:  # check if h is less than 2 * eps_mach
+            iflag = -1
+            msg = "Length of h is zero. Check the spline data."
+            CubicSpline._log_spline(iflag, msg, log_file=log_file)
+            return None
+
+        # Evaluation in t
+        yi = self.ylist[i]
+        yii = self.ylist[i + 1]
+        Mi = self.Mlist[i]
+        Mii = self.Mlist[i + 1]
+        diff1 = t - xi
+        diff2 = xii - t
+        val = diff1 * (yii / h - Mii * h / 6. + diff1 * diff1 * Mii /
+                       (6. * h)) + diff2 * (yi / h - Mi * h / 6. + diff2 *
+                                            diff2 * Mi / (6. * h))
+
+        return val
+
+    def read(self, filename):
+        """Read the spline data from file.
+
+        Assumptions:
+            -- The file is written as that printed with __str__"""
+        with open(filename, 'r') as fh:
+            contents = fh.readlines()
+        # Extract numbers
+        search_filter = r"[-+]?\d*\.\d+|\d+"
+        self.xlist = [float(i) for i in re.findall(search_filter, contents[1])]
+        self.ylist = [float(i) for i in re.findall(search_filter, contents[2])]
+        self.Mlist = [float(i) for i in re.findall(search_filter, contents[3])]
+
     def reset(self):
-        """Resets all the attributes of the spline instance."""
+        """Reset all the attributes of the spline instance."""
         self.xlist = []
         self.ylist = []
         self.Mlist = []
 
 
 if __name__ == '__main__':
-    seed(0)
-    xvals = [random() for _ in range(5)]
-    yvals = [random() for _ in range(5)]
-    Mvals = [random() for _ in range(5)]
+    # seed(0)
+    # xvals = [random() for _ in range(5)]
+    # yvals = [random() for _ in range(5)]
+    # Mvals = [random() for _ in range(5)]
+    xvals = [0., 1., 3., 4.5, 5.]
+    yvals = [2., -1., 0.5, 3., 1.]
+    Mvals = [0., 1., 3., 0., -1.]
     myspline = CubicSpline(xvals, yvals, Mvals)
-    print(repr(myspline))
-    print(myspline)
+    myspline.eval(4.6)
 
-    print(myspline.dict_form)
-
-    mydict = {
-        'xlist': [1., 2., 3.],
-        'ylist': [1.1, 2.2, 3.3],
-        'Mlist': [0., 0., 0.]
-    }
-
-    myspline.dict_form = mydict
-    print(myspline)
-    myspline.reset()
-    print(myspline.dict_form)
+    # print(repr(myspline))
+    # print("Before modifying the spline.")
+    # print(myspline)
+    # with open("dummy.txt", 'w') as fh:
+    #     print(myspline, file=fh)
+    # myspline.dict_form = {
+    #     'xlist': [1.],
+    #     'ylist': [1.],
+    #     'Mlist': [1.]
+    # }
+    # print("After modifying the spline.")
+    # print(myspline)
+    # myspline.read("dummy.txt")
+    # print("After reading the original spline file.")
+    # print(myspline)
+    # os.remove('dummy.txt')
+    #
+    # print(myspline.dict_form)
+    #
+    # mydict = {
+    #     'xlist': [1., 2., 3.],
+    #     'ylist': [1.1, 2.2, 3.3],
+    #     'Mlist': [0., 0., 0.]
+    # }
+    #
+    # myspline.dict_form = mydict
+    # print(myspline)
+    # myspline.reset()
+    # print(myspline.dict_form)
--- tests/test_splinelib.py
+++ tests/test_splinelib.py
@@ -27,21 +27,21 @@ class TestCubicSpline(unittest.TestCase):
         self.xvals = [rnd.random() for _ in range(5)]
         self.yvals = [rnd.random() for _ in range(5)]
         self.Mvals = [rnd.random() for _ in range(5)]
-        self.myspline = csp.CubicSpline(self.xvals, self.yvals, self.Mvals)
+        self.test_spline = csp.CubicSpline(self.xvals, self.yvals, self.Mvals)
         print(f"Executing {self._testMethodName}")
 
     def test_len(self):
-        self.assertEqual(len(self.myspline), 5)
+        self.assertEqual(len(self.test_spline), 5)
 
     def test_repr(self):
-        self.assertEqual(repr(self.myspline),
+        self.assertEqual(repr(self.test_spline),
                          f"CubicSpline('{self.xvals}, {self.yvals}, {self.Mvals}')")
 
     def test_str(self):
         """Writes the spline attributes on file, then reads them, parses the
         contents and compare them with the actual splines attributes."""
         with open('test_print.txt', 'w') as fh:
-            print(self.myspline, file=fh)
+            print(self.test_spline, file=fh)
         with open('test_print.txt', 'r') as fh:
             contents = fh.readlines()
         # Extract numbers
@@ -51,18 +51,110 @@ class TestCubicSpline(unittest.TestCase):
         y_printed = re.findall(search_filter, contents[2])
         M_printed = re.findall(search_filter, contents[3])
         # Check
-        xdiff = [np.abs(float(x_printed[i]) - self.myspline.xlist[i]
-                        ) < 1e-4 for i in range(len(self.myspline))]
-        ydiff = [np.abs(float(y_printed[i]) - self.myspline.ylist[i]
-                        ) < 1e-4 for i in range(len(self.myspline))]
-        Mdiff = [np.abs(float(M_printed[i]) - self.myspline.Mlist[i]
-                        ) < 1e-4 for i in range(len(self.myspline))]
-        self.assertEqual(int(len_printed[0]), len(self.myspline))
+        xdiff = [np.abs(float(x_printed[i]) - self.test_spline.xlist[i]
+                        ) < 1e-4 for i in range(len(self.test_spline))]
+        ydiff = [np.abs(float(y_printed[i]) - self.test_spline.ylist[i]
+                        ) < 1e-4 for i in range(len(self.test_spline))]
+        Mdiff = [np.abs(float(M_printed[i]) - self.test_spline.Mlist[i]
+                        ) < 1e-4 for i in range(len(self.test_spline))]
+        self.assertEqual(int(len_printed[0]), len(self.test_spline))
         self.assertTrue(all(xdiff))
         self.assertTrue(all(ydiff))
         self.assertTrue(all(Mdiff))
         # Clean-up
         os.remove('test_print.txt')
+
+    def test_dict_form(self):
+        test_spline = csp.CubicSpline([1., 2., 3.], [4., 5., 6.], [7., 8., 9.])
+        self.assertEqual(test_spline.dict_form, {'xlist': [1., 2., 3.],
+                                                 'ylist': [4., 5., 6.],
+                                                 'Mlist': [7., 8., 9.]})
+
+    def test_dict_form_setter(self):
+        self.test_spline.dict_form = {'xlist': [11., 22., 33.],
+                                      'ylist': [44., 55., 66.],
+                                      'Mlist': [77., 88., 99.]}
+        self.assertEqual(self.test_spline.xlist, [11., 22., 33.])
+        self.assertEqual(self.test_spline.ylist, [44., 55., 66.])
+        self.assertEqual(self.test_spline.Mlist, [77., 88., 99.])
+
+    def test_eval(self):
+        test_x = [0., 1., 3., 4.5, 5.]
+        test_y = [2., -1., 0.5, 3., 1.]
+        test_M = [0., 1., 3., 0., -1.]
+        test_points = [1., 1.5, 1.7, 2.3, 4.6]
+        ref_vals = [-1., -1.3125, -1.3395, -0.9805, 2.608]
+        # Create the spline
+        test_spline = csp.CubicSpline(test_x, test_y, test_M)
+        # Do the checks
+        for i, point in enumerate(test_points):
+            val = test_spline.eval(point)
+            self.assertAlmostEqual(val, ref_vals[i], delta=1e-4,
+                                   msg=f"Value in {point} should be {ref_vals[i]}, while it is {val}")
+
+    def test_eval_empty(self):
+        test_x = []
+        test_y = []
+        test_M = []
+        # Create the spline
+        test_spline = csp.CubicSpline(test_x, test_y, test_M)
+        # Do the check
+        self.assertIsNone(test_spline.eval(1.))
+
+    def test_eval_single_point(self):
+        test_x = [1.]
+        test_y = [1.]
+        test_M = [1.]
+        # Create the spline
+        test_spline = csp.CubicSpline(test_x, test_y, test_M)
+        # Do the check
+        self.assertEqual(test_spline.eval(4.9), test_x[0])
+
+    def test_eval_extrapolation_left(self):
+        test_x = [0., 1., 3., 4.5, 5.]
+        test_y = [2., -1., 0.5, 3., 1.]
+        test_M = [0., 1., 3., 0., -1.]
+        # Create the spline
+        test_spline = csp.CubicSpline(test_x, test_y, test_M)
+        # Do the checks
+        self.assertIsInstance(test_spline.eval(-0.5), float)
+
+    def test_eval_extrapolation_right(self):
+        test_x = [0., 1., 3., 4.5, 5.]
+        test_y = [2., -1., 0.5, 3., 1.]
+        test_M = [0., 1., 3., 0., -1.]
+        # Create the spline
+        test_spline = csp.CubicSpline(test_x, test_y, test_M)
+        # Do the checks
+        self.assertIsInstance(test_spline.eval(6.), float)
+
+    def test_eval_zero_interval(self):
+        test_x = [1., 1.]
+        test_y = [2., 2.]
+        test_M = [3., 3.]
+        # Create the spline
+        test_spline = csp.CubicSpline(test_x, test_y, test_M)
+        # Do the check
+        self.assertIsNone(test_spline.eval(5.))
+
+    def test_read(self):
+        text = """Cubic spline. Length: 3
+        xlist: 1. 1. 1.
+        ylist: 2. 2. 2.
+        Mlist: 3. 3. 3.
+        """
+        with open('test_read.txt', 'w') as fh:
+            fh.write(text)
+        self.test_spline.read('test_read.txt')
+        self.assertEqual(self.test_spline.xlist, [1., 1., 1.])
+        self.assertEqual(self.test_spline.ylist, [2., 2., 2.])
+        self.assertEqual(self.test_spline.Mlist, [3., 3., 3.])
+
+    def test_reset(self):
+        self.test_spline.reset()
+        self.assertEqual(self.test_spline.xlist, [])
+        self.assertEqual(self.test_spline.ylist, [])
+        self.assertEqual(self.test_spline.Mlist, [])
 
 
 if __name__ == '__main__':
